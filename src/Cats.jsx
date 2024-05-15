095e835e5722d0bb50e159720372589595024b5b
Jjaclyn Alday <jaalday123@gmail.com>
2024-05-14 21:45:25 -0400

    added cats

--- src/Cats.jsx
+++ src/Cats.jsx
@@ -25,10 +25,10 @@ const Cats = () => {
           <img className="img-one"src="https://www.rd.com/wp-content/uploads/2021/04/GettyImages-960998894-e1692805244605.jpg"/>
         </div>
         <div className="four">
-          <p>cat 4</p>
+          <img className="img-one" src="https://hips.hearstapps.com/hmg-prod/images/cat-captions-for-instagram-6595a058e956b.jpeg"/>
         </div>
         <div className="five">
-          <p>cat 5</p>
+          <img className="img-one" src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-domestic-cat-mjf-1540-382ba2.jpg"/>
         </div>
         <div className="six">
           <p>cat 6</p>
