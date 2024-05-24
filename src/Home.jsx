044f1c274259439ff58df45b7787b49fce3af706
Jjaclyn Alday <jaalday123@gmail.com>
2024-05-24 15:02:48 -0400

    home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import './App.css'
-import Cats from './Cats'
-import About from './About'
+
 
 
 
@@ -17,8 +16,34 @@ function Home() {
     <p className='para'>Where cats and coffee come together</p>
     </div>
     </div>
-    <Cats/>
-    <About/>
+    <div className='imgBoard'>
+      <div className='box1'>
+  
+    <img className="boximg" src="https://images.squarespace-cdn.com/content/v1/6341c2040cf2f92554f570f1/d9746885-61ed-4a4f-a867-464497bd7843/CAM_2231.jpg"/>
+    </div>
+    <div className='box2'>
+    <div className='textmiddle'>
+      <h3 className='bio'>Our Story</h3>
+      <p className='para2'>Our cat cafe provides a space to relax, cuddle with adorably sweet cats, and enjoy delicious drinks and snacks. Our establishment partners with local shelters and foster programs to help home and expose cats to potential adopters.</p>
+      </div>
+     <img className="boximg2" src="https://m.media-amazon.com/images/I/91gjASP--DL._AC_UF1000,1000_QL80_.jpg"/>
+  
+
+    </div>
+    </div>
+    <div className='container2'>
+      <div className='containerbox'>
+        <img className="img2"src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=1200,height=1200,fit=cover/article/main-picture/6287b98e9cbf8439821570.jpg" alt="cats playing"/>
+      </div>
+      <div className='containerbox2'>
+        <img className='img2' src="https://hips.hearstapps.com/hmg-prod/images/summer-dessert-recipes-no-bake-strawberry-cheesecake-re-66181f77de61e.jpeg?crop=1xw:1xh;center,top&resize=980:*" alt="Delicious Cheesecake"/>
+      </div>
+      <div className='containerbox3'>
+        <img className='img2' src="https://img.freepik.com/premium-photo/coffee-shop-with-large-wooden-table-chairs-large-window-with-flower-it_900958-31200.jpg"/>
+      </div>
+
+    </div>
+   
   
     
  
