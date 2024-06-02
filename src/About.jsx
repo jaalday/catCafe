2e47bcfebb91002f956cb6d16f7ba9e46e9396f6
Jjaclyn Alday <jaalday123@gmail.com>
2024-06-02 14:05:54 -0400

    about change

--- src/About.jsx
+++ src/About.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const About = () => {
     return (
+        <>
         <div className='about'>
             <img className='img3' src='https://www.freewebheaders.com/wp-content/gallery/cats/lovely-american-shorthair-kitten-website-header.jpg'/>
             <div className="aboutText">
@@ -12,6 +13,10 @@ const About = () => {
             <h4> Each of our cats are from local rescues and are available for adoption.  We partner with local shelters to provide a space for cats to get more exposure to socilization and increase their chance for adoption. </h4>
             <h4>We have a variety of snacks and drinks to choose from, feel free to view our <Link to="/menu">menu! </Link></h4></div>
         </div>
+        <div>
+
+        </div>
+        </>
     );
 }
 export default About;
